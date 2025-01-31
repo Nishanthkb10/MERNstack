39f77011dad6b718d991d76907cc5de84575ff1c
Nishanthkb10 <nishanth.kb.10@gmail.com>
2025-01-31 16:21:54 +0530

    updated

--- Backend/config/config.js
+++ Backend/config/config.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
   try {
-    await mongoose.connect("mongodb+srv://nishanthkb:password@cluster0.jsa4r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    await mongoose.connect("mongodb+srv://nishanthkb:password_@cluster0.jsa4r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
     console.log("Connected to DB");
   } catch (error) {
     console.log("Error connecting DB: ", error.message);
